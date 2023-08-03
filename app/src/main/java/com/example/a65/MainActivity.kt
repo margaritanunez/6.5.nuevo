1fe5268d7b012c0659dc61a56f0d84ba15565556
Margaret <maenunezt@gmail.com>
2023-08-03 06:55:16 -0400

    creación del proyecto

---
+++ app/src/main/java/com/example/a65/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.a65
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/*
+Pasos para hacer esta cosa:
+1.. Commit inicial
+
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
