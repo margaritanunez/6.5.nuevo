9828aa2489cf98e1454ca698f5d4b807d30dfe9d
Margaret <maenunezt@gmail.com>
2023-08-03 11:03:23 -0400

    implementación de repositorio en paquete data, creación de paquete "presentation" , en donde se implmeentan la clase vm y se construye un List Fragment. También se agregan las dependencias de view model en build.gradle

--- app/src/main/java/com/example/a65/MainActivity.kt
+++ app/src/main/java/com/example/a65/MainActivity.kt
@@ -12,6 +12,12 @@ Pasos para hacer esta cosa:
     5.1.. POJO... construir data class. Aquí es dónde necesitamos traducir los elementos de la api a dataclass
     5.2.. interfaz de operaciones, crear una nueva interface
     5.3.. cliente de retrofit, hacer una clase nueva que se llame retrofit client.
+6.. Crear clase repositorio, pero solo en el paquete data
+7.. Crear package presentation, en dónde irá clase VM
+8.. Agregar dependencia de VM
+9.. Seguir con VM
+10.. Construir un ListFragment en el package presentation
+
  */
 
 class MainActivity : AppCompatActivity() {
